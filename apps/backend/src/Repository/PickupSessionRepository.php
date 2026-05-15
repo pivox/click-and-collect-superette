12d51ed549e033e1a075d3f58492d1d643072a99
Haythem Mabrouk <haythem.mabrouk@gmail.com>
2026-05-15 08:32:29 +0200

    feat(sprint4): add pickup session repository

---
+++ apps/backend/src/Repository/PickupSessionRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Order;
+use App\Entity\PickupSession;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @extends ServiceEntityRepository<PickupSession>
+ */
+class PickupSessionRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PickupSession::class);
+    }
+
+    public function findOneByOrder(Order $order): ?PickupSession
+    {
+        return $this->findOneBy(['order' => $order]);
+    }
+
+    public function findOneByToken(Uuid $token): ?PickupSession
+    {
+        return $this->findOneBy(['token' => $token]);
+    }
+}
